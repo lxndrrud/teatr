f729027029165d3a0ff0a1116b62cecd5e1a39d4
lxndrrud <lxndrrud@gmail.com>
2022-03-25 13:12:26 +0300

    objection starting

--- expressjs/knex/connections.ts
+++ expressjs/knex/connections.ts
@@ -1,4 +1,6 @@
-import knex from "knex"
+import knex, { Knex } from "knex"
 import { config } from "../knexfile.type"
+import { Model }  from "objection"
 
 export const KnexConnection = knex(config.main)
+export const model = Model.knex(knex(config.main))
