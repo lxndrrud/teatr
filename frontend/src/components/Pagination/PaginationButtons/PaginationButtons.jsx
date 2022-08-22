0af19c0f0da5066e50566c370762cd3394c1b718
lxndrrud <lxndrrud@yandex.ru>
2022-08-22 10:38:51 +0300

    pagintation button styling

--- frontend/src/components/Pagination/PaginationButtons/PaginationButtons.jsx
+++ frontend/src/components/Pagination/PaginationButtons/PaginationButtons.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 function PaginationButtons({ activePage, setActivePage, lastPage }) {
     return (
         <div className='flex flex-row justify-center'>
-            <button onClick={() => setActivePage(activePage-1)} disabled={activePage===1} >
+            <button className="flex p-3 border-solid border-1 border-black bg-[#ae2876]" 
+                onClick={() => setActivePage(activePage-1)} disabled={activePage===1} >
                 {'<'}
             </button>
-            <button onClick={() => setActivePage(activePage+1)} disabled={activePage===lastPage}  >
+            <button className="" 
+                onClick={() => setActivePage(activePage+1)} disabled={activePage===lastPage}  >
                 {'>'}
             </button>
         </div>
